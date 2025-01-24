024d1f7417c6ddc319dbbac2d68cb04d9c7c5490
angiez37 <angelsaka@yahoo.com>
2025-01-24 09:27:18 -0500

    maze solver class containing move forward mechanism to solve a simple maze. close #7, close #8

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolver.java
@@ -1,5 +1,23 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
-public interface MazeSolver {
-    MazeSolver find_path(Maze maze);
+public MazeSolver(Maze maze) {
+    this.maze = maze;
+    int[] entry = maze.findEntryPoint();
+    if (entry != null) {
+        this.row = entry[0];
+        this.col = entry[1];
+    }
+}
+
+public void solve() {
+    while (col < maze.getCols() - 1 && !maze.isWall(row, col)) {
+        moveForward();
+    }
+}
+
+private void moveForward() {
+    if (!maze.isWall(row, col)) {
+        maze.setTile(row, col, Tile.PATH); // Mark the path
+        col++; // Move right in a straight maze
+    }
 }
