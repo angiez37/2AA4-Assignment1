7de8dda385f94802a688dee1c59c9e7218e22a94
angiez37 <angelsaka@yahoo.com>
2025-01-23 23:34:01 -0500

    removed righthandAlgo and MazeProcessor classes to make MVP iteration simpler

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandAlgo.java
+++
@@ -1,9 +0,0 @@
-package ca.mcmaster.se2aa4.mazerunner;
-
-import ca.mcmaster.se2aa4.mazerunner.MazeSolver;
-
-public class RightHandAlgo implements MazeSolver {
-    public MazePath find_path(Maze maze) {
-        return new MazeProcessor(10, "10F");
-    }
-}
